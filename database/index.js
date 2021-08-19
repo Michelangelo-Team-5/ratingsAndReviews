257e5228e33e0028cd7c082133ffc0e192821385
Keefe Dashiell <kldashing@gmail.com>
2021-08-19 13:09:39 -0700

    changed server

--- database/index.js
+++ database/index.js
@@ -1,11 +1,21 @@
 const pgp = require('pg-promise')();
 // 'postgres://username:password@localhost:5432/reviews'
 
+// const cn = {
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'reviews',
+// };
+
+
 const cn = {
-  host: 'localhost',
+  host: '18.117.247.126',
   port: 5432,
   database: 'reviews',
+  user: 'postgres',
+  password: 'test'
 };
+
 
 const db = pgp(cn);
 
--- server/index.js
+++ server/index.js
@@ -1,38 +1,35 @@
 const express = require('express');
 const path = require('path');
-const router = require('./routes.js')
+const router = require('./routes.js');
 const cluster = require('cluster');
 const totalCPUs = require('os').cpus().length;
 
-if (cluster.isMaster) {
-  console.log(`Number of CPUs is ${totalCPUs}`);
-  console.log(`Master ${process.pid} is running`);
+// if (cluster.isMaster) {
+//   console.log(`Number of CPUs is ${totalCPUs}`);
+//   console.log(`Master ${process.pid} is running`);
 
-  for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
-  }
+//   for (let i = 0; i < totalCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    console.log("Let's fork another worker!");
-    cluster.fork();
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//     console.log("Let's fork another worker!");
+//     cluster.fork();
+//   });
+// }
+  const PORT = 3000;
+  const app = express();
+
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+  app.use(express.json());
+  app.use('/api', router);
+
+  // app.get('/', (req, res) => {
+  //   res.send('Hello from the server!');
+  // });
+
+  app.listen(PORT, () => {
+    console.log(`Server listening at localhost:${3000}!`);
   });
 
-} else {
-
-    const PORT = 3000;
-    const app = express();
-
-    app.use(express.static(path.join(__dirname, '..', 'public')));
-    app.use(express.json());
-    app.use('/api', router);
-
-    // app.get('/', (req, res) => {
-    //   res.send('Hello from the server!');
-    // });
-
-    app.listen(PORT, () => {
-      console.log(`Server listening at localhost:${3000}!`);
-    });
-
-}
